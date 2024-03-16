d94b19cee0ea49d01107a4074b3e2ab0c05db455
rbshah2712 <34372610+rbshah2712@users.noreply.github.com>
2024-03-16 10:30:24 -0400

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { of,map, from, filter, concat, merge } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'obs_depth_method';
+ 
+  title1 = "Observable Methods";
+  
+  users = from([
+    {id:1,title:'Ruchi'},
+    {id:2,title:'Bhavin'}
+  ]);
+
+  employee = from([
+    {id:1,name:'Ruchi'},
+    {id:2,name:'Bhavin'}
+  ]);
+
+  array1 = [5,10,13,4,5,6];
+  array2 = [3,'A','B','C'];
+
+  newobser = from(this.array1);
+  newobser1 = from(this.array2);
+
+  namesObservable = this.users.pipe(
+    map((obj:any) => "Mr "+obj.title)
+  ).subscribe((title) => {
+    console.log(`Title: ${title}`);
+  });
+
+  transformed = this.newobser.pipe(map((val) => {
+    return val * 5;
+  }
+  ))
+
+  filteredobs = this.transformed.pipe(filter((val) => {
+    return val  >= 30
+  } 
+  ))
+
+  concateobs = concat(this.newobser,this.newobser1);
+  mergeobs = merge(this.newobser,this.newobser1);
+
+ ngOnInit(){
+  this.filteredobs.subscribe((val) => {
+    console.log('the filtered:  '+val);
+  },(error) => {
+     alert(error)
+  });
+
+  this.concateobs.subscribe(x => console.log('concated array:   '+x));
+  this.mergeobs.subscribe(x => console.log('merged array:   '+x));
+
+ }
+
+  
 }
